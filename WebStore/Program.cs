cdcd31d5695677a8073ce28dd57c4f27465b46d3
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-17 08:50:09 +0300

    Добавьте файлы проекта.

---
+++ WebStore/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace WebStore
+{
+    internal partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
+
+
