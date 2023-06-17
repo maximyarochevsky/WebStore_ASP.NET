9e75f7849f2421070b368c603499b9797dd365fa
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-17 10:27:16 +0300

    Добавление таблицы с сотрудниками

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        public static readonly List<Employee> __Employees = new()
+        {
+            new Employee{Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 18 },
+            new Employee{Id = 2, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 19 },
+            new Employee{Id = 3, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 20 }
+        };
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult ConfiguredAction()
+        public IActionResult Employees()
         {
-            return 
+            return View(__Employees);
         }
     }
 }
