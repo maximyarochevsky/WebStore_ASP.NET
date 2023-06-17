42adb595bd9c4fe63cf186ece46332a68449034b
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-17 09:20:29 +0300

    Добавлена система маршрутизации, страница с инофрмацией об ошибке

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -7,10 +7,23 @@ namespace WebStore
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var services = builder.Services;
+            services.AddControllersWithViews();
             var app = builder.Build();
+           
+            if (app.Environment.IsDevelopment())
+            {
+            app.UseDeveloperExceptionPage();
+            }
 
+            app.UseRouting();
             app.MapGet("/", () => "Hello World!");
+            app.MapGet("/throw", () =>
+            {
+                throw new Exception("Ошибка!");
+            });
 
+            app.MapDefaultControllerRoute();
             app.Run();
         }
     }
