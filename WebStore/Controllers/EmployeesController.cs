f5e2755f5e9a369f83b0039766ef2603d767a97a
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-17 12:37:21 +0300

    Разделение макета Index на состовляющие и создание контроллера для сотрудников

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public static readonly List<Employee> __Employees = new()
+        {
+            new Employee{Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 18 },
+            new Employee{Id = 2, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 19 },
+            new Employee{Id = 3, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 20 }
+        };
+        public IActionResult Index()
+        {
+            return View(__Employees);
+        }
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -5,12 +5,7 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        public static readonly List<Employee> __Employees = new()
-        {
-            new Employee{Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 18 },
-            new Employee{Id = 2, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 19 },
-            new Employee{Id = 3, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 20 }
-        };
+     
         public IActionResult Index()
         {
             return View();
