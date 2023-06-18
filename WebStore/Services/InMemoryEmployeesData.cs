bd500c186392ce570f6089de32a0857c25a11e97
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-18 18:47:13 +0300

    Работа над интерфейсом и сервисом для редактирования сотрудников

---
+++ WebStore/Services/InMemoryEmployeesData.cs
@@ -0,0 +1,39 @@
+﻿using WebStore.Models;
+using WebStore.Services.Interfaces;
+using WebStore.Data;
+
+namespace WebStore.Services
+{
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+        private readonly ICollection<Employee> _Employees;
+        public InMemoryEmployeesData()
+        {
+            _Employees = TestData.__Employees;
+        }
+        public int Add(Employee employee)
+        {
+            
+        }
+
+        public bool Delete(int id)
+        {
+           
+        }
+
+        public bool Edit(Employee employee)
+        {
+           
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _Employees;
+        }
+
+        public Employee GetById(int id)
+        {
+            return _Employees.FirstOrDefault(employee => employee.Id == id);
+        }
+    }
+}
