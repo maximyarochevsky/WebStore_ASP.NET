151ea7495a4fb98fda9ea4860c5355c01363f502
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-18 18:28:04 +0300

    Заготовка методов редактирования в контроллере сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
 using WebStore.Models;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
-    [Route("Staff/{action=Index}/{Id?}")]
+    //[Route("Staff/{action=Index}/{Id?}")]
     public class EmployeesController : Controller
     {
-        public static readonly List<Employee> __Employees = new()
+        private ICollection<Employee> __Employees;
+
+        public EmployeesController()
         {
-            new Employee{Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 18 },
-            new Employee{Id = 2, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 19 },
-            new Employee{Id = 3, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 20 }
-        };
+            __Employees = TestData.__Employees;
+        }
         public IActionResult Index()
         {
             var result = __Employees;
@@ -30,5 +32,33 @@ namespace WebStore.Controllers
             ViewBag.SelectedEmployee = employee;
             return View(employee);
         }
+
+        public IActionResult Create() => View();
+
+        public IActionResult Edit(int id)
+        {
+            var employee = __Employees.FirstOrDefault(e => e.Id == id);
+            if (employee is null)
+                return NotFound();
+
+            var model = new EmployeeEditViewModel
+            {
+                Id = employee.Id,
+                LastName = employee.LastName,
+                FirstName = employee.FirstName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age,
+            };
+
+            return View();
+        }
+
+        public IActionResult Edit(EmployeeEditViewModel Model)
+        {
+            //Обработка модели...
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id) => View();
     }
 }
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,14 @@
+﻿using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public class TestData
+    {
+        public static List<Employee> __Employees { get; } = new()
+        {
+            new Employee{Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 18 },
+            new Employee{Id = 2, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 19 },
+            new Employee{Id = 3, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 20 }
+        };
+    }
+}
