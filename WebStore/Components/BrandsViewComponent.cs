c5d592ccaabb9910498d943a10db25f4ab63b005
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-20 14:06:04 +0300

    Компонент брэндов

--- WebStore/Components/BrandsViewComponent.cs
+++ WebStore/Components/BrandsViewComponent.cs
@@ -4,14 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components
 {
     public class BrandsViewComponent: ViewComponent
     {
+        private readonly IProductData _ProductData;
+
+        public BrandsViewComponent(IProductData ProductData)
+        {
+            _ProductData = ProductData;
+        }
         public IViewComponentResult Invoke()
         {
-            return View();
+            return View(GetBrands());
+        }
+
+        private IEnumerable<BrandViewModel> GetBrands()
+        { 
+          return  _ProductData.GetBrands().OrderBy(b => b.Order).Select(b => new BrandViewModel
+            {
+                Id = b.Id,
+                Name = b.Name,
+            });
         }
     }
 }
