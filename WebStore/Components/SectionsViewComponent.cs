3e7ee439e75ac8dfb43a7f02807f7e5f31e934d9
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-20 13:08:32 +0300

    Модель-представления секции

---
+++ WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Components;
+
+public class SectionsViewComponent : ViewComponent
+{
+	private readonly IProductData _ProductData;
+
+    public SectionsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+	
+    public IViewComponentResult Invoke()
+    {
+        var sections = _ProductData.GetSections();
+
+        return View();
+    }
+}
