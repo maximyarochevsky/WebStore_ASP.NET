c3152422beaedddfe92e2e4b17013b082340bee6
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-21 10:43:36 +0300

    Настройка контроллера каталога товаров и настройка котроллера Home для их отображения

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebStore.Models;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
      
-        public IActionResult Index()
+        public IActionResult Index([FromServices]IProductData ProductData)
         {
+            var products = ProductData.GetProducts()
+                .OrderBy(p=>p.Order)
+                .Take(6)
+                .Select(
+                    p => new ProductViewModel
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        Price = p.Price,
+                        ImageUrl = p.ImageUrl,
+                    });
+            ViewBag.Products = products;
             return View();
         }
     }
