53754ee2f84a9430f99ce26aad727809522067b3
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-24 19:16:26 +0300

    Контроллер регистрации

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModels.Identity;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+		public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        {
+                _SignInManager = SignInManager;
+                _UserManager = UserManager; 
+        } 
         public IActionResult Register()
         {
-            return View();
+            return View(new RegisterUserViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterUserViewModel Model)
+        {
+            if (ModelState.IsValid)
+                return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName,
+            };
+
+            var registraion_result = await _UserManager.CreateAsync(user, Model.Password);
+            if (registraion_result.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index","Home");
+            }
+
         }
         public IActionResult Login()
         {
@@ -14,7 +46,7 @@ namespace WebStore.Controllers
         }
         public IActionResult Logout()
         {
-            return View();
+            return RedirectToAction("Index", "Home");
         }
         public IActionResult AccessDenied()
         {
