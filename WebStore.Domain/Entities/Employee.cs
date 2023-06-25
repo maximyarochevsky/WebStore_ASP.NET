60a1959b1dcd74e868f605b05d90d1260ad7c8d3
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-25 13:24:37 +0300

    Сотрудники - это сущность

--- WebStore.Domain/Entities/Employee.cs
+++ WebStore.Domain/Entities/Employee.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base;
 
 namespace WebStore.Domain.Entities;
 
-public class Employee
+public class Employee : Entity
 {
-	public int Id { get; set; }
 	public string LastName { get; set; }
 	public string FirstName { get; set; }
 	public string Patronymic { get; set; }
