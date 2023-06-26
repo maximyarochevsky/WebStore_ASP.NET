6df43410678f05aa0b2216ea9f0c4c493b00f159
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-26 13:06:10 +0300

    Визуальный компонент отображения информации о пользователе в заголовке страницы

---
+++ WebStore/Views/Shared/Components/UserInfo/UserInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebStore.Views.Shared.Components.UserInfo
+{
+    public class UserInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
