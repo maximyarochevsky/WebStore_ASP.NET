998ffa24af282e22648275527db6deb4512442fb
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-27 16:07:35 +0300

    Сущность заказа

--- WebStore.Domain/Entities/Orders/Order.cs
+++ WebStore.Domain/Entities/Orders/Order.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Identity;
 
-namespace WebStore.Domain.Entities.Orders
+namespace WebStore.Domain.Entities.Orders;
+
+public class Order
 {
-    internal class Order
-    {
-    }
+    [Required]
+    public User User { get; set; } = null;
+
+    [Required]
+    [MaxLength(200)]
+    public string Phone { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Adress { get; set; }
+
+    public string Descripction { get; set; }
+
+    public DateTimeOffset Date { get; set; } = DateTimeOffset.Now;
+
+    public ICollection<OrderItem> Items { get; set} = new List<OrderItem>();
+
+    [NotMapped]
+    public decimal TotalPrice => Items.Sum(item => item.TotalItemPrice);
 }
--- WebStore.Domain/Entities/Orders/OrderItem.cs
+++ WebStore.Domain/Entities/Orders/OrderItem.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
 
-namespace WebStore.Domain.Entities.Orders
+namespace WebStore.Domain.Entities.Orders;
+
+public class OrderItem : Entity
 {
-    internal class OrderItem
-    {
-    }
+    [Required]
+    public Product Product { get; set; } = null!;
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+
+    public int Quantity { get; set; }
+
+    public Order Order { get; set; } = null!;
+
+    [NotMapped]  //EF не добавляет это свойство в БД
+    public decimal TotalItemPrice => Price * Quantity;
 }
