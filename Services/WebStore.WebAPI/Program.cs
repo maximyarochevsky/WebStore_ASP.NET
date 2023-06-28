8d5f6e668e956c6a8a303da9c31afbe5954a5c83
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-28 13:55:17 +0300

    Тестовое WebAPI

--- Services/WebStore.WebAPI/Program.cs
+++ Services/WebStore.WebAPI/Program.cs
@@ -1,11 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var services = builder.Services;    
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 var app = builder.Build();
 
