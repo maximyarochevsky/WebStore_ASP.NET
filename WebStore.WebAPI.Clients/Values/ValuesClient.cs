b26d9c7c99d2aeee1063edc1e878767713b4acce
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-28 16:56:55 +0300

    Заготовка клиента тестового WebAPI

--- WebStore.WebAPI.Clients/Values/ValuesClient.cs
+++ WebStore.WebAPI.Clients/Values/ValuesClient.cs
@@ -1,39 +1,45 @@
 ﻿using WebStore.Interfaces.TestAPI;
+using WebStore.WebAPI.Clients.Base;
 
-namespace WebStore.WebAPI.Clients.Values
+namespace WebStore.WebAPI.Clients.Values;
+
+public class ValuesClient : BaseClient, IValuesService
 {
-    public class ValuesClient : IValuesService
-
+    public ValuesClient(HttpClient Client) : base(Client, "api/values")
     {
-        private HttpClient _Client;
-        public void Add(string value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Count()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Delete(int Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Edit(int Id, string value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string? GetById(int Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<string> GetValues()
-        {
-            throw new NotImplementedException();
-        }
     }
+
+    private HttpClient _Client;
+    public void Add(string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Count()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Edit(int Id, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string? GetById(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<string> GetValues()
+    {
+        throw new NotImplementedException();
+    }
+
+   
 }
+
