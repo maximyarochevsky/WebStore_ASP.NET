3cf16fbeb7e608404b22d704d381d0106d725eba
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-28 16:54:31 +0300

    Класс базового клиента WebAPI

---
+++ WebStore.WebAPI.Clients/Values/ValuesClient.cs
@@ -0,0 +1,39 @@
+﻿using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.WebAPI.Clients.Values
+{
+    public class ValuesClient : IValuesService
+
+    {
+        private HttpClient _Client;
+        public void Add(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(int Id, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
