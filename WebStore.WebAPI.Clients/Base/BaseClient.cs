3b86d99432ba3fdf39dd581b9b8aafc46de56f93
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-28 16:59:40 +0300

    Базовый класс клиента

--- WebStore.WebAPI.Clients/Base/BaseClient.cs
+++ WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebStore.WebAPI.Clients.Base
-{
-    internal class BaseClient
+﻿namespace WebStore.WebAPI.Clients.Base;
+public abstract class BaseClient
     {
+    protected HttpClient Http { get; set; }
+    protected string Address { get; }
+
+    protected BaseClient(HttpClient Client, string Address)
+    {
+        Http = Client;
+        this.Address = Address;
     }
-}
+    }
+
