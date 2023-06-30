ac74de9da8b4abadb63058066fc25a9e2dd65656
maximyarochevsky <maximyarochevskygithub@gmail.com>
2023-06-30 10:24:05 +0300

    Клиент для WebAPI управления сотрудниками

--- WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
+++ WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
@@ -1,12 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Http.Json;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
 
-namespace WebStore.WebAPI.Clients.Employees
+namespace WebStore.WebAPI.Clients.Employees;
+
+public class EmployeesClient : BaseClient, IEmployeesData
 {
-    internal class EmployeesClient
+    public EmployeesClient(HttpClient Client) : base(Client, "api/employees")
     {
     }
+
+    public int Add(Employee employee)
+    {
+        var response = Post(Address, employee);
+        var added_employee = response.Content.ReadFromJsonAsync<Employee>().Result;
+
+        if (added_employee is null)
+            return -1;
+
+        var id = added_employee.Id;
+        employee.Id = id;
+
+        return id;
+    }
+
+    public bool Delete(int id)
+    {
+        var response = Delete($"{Address}/{id}");
+        var success = response.IsSuccessStatusCode;
+        return success;
+    }
+
+    public bool Edit(Employee employee)
+    {
+        var response = Put(Address, employee);
+        var success = response.EnsureSuccessStatusCode()
+            .Content
+            .ReadFromJsonAsync<bool>()
+            .Result;
+
+        return success;
+    }
+
+    public IEnumerable<Employee> GetAll()
+    {
+        var employees = Get<IEnumerable<Employee>>(Address);
+        return employees!;
+    }
+
+    public Employee? GetById(int id)
+    {
+        var result = Get<Employee>($"{Address}/{id}");
+        return result;
+    }
 }
+
